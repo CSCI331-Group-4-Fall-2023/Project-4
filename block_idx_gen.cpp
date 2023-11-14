5dde97ea7c2e4d46aa6d0a9a54641db47bb3ba5e
Andrew Clayton <126101983+andrew-clayton@users.noreply.github.com>
2023-11-14 08:49:32 -0600

    Index generation updated
    This is a cleaner version of the file, without tons of troubleshooting steps in the code.

--- block_idx_gen.cpp
+++ block_idx_gen.cpp
@@ -4,7 +4,7 @@
  * @brief This class creates an index file for a blocked data file. 
  * @author Andrew Clayton
  * @date 11/13/2023
- * @version 1.0
+ * @version 1.2
  */
 // ----------------------------------------------------------------------------
 /**
@@ -30,7 +30,6 @@ int findZipcode(const string& record) {
         cerr << "Error parsing record for zipcode: " << record << "\n";
         return -1;
     }
-    // cout << "Zipcode: " << record.substr(firstComma + 1, secondComma - firstComma - 1) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
     return stoi(record.substr(firstComma + 1, secondComma - firstComma - 1));
 }
 
@@ -52,20 +51,6 @@ int main() {
     int maxZipcode = 0;
     string currentBlock;
 
-    // testing...
-    // getline(readFile, currentBlock);
-    // cout << "\n" << currentBlock << "\n\n";
-    // int startOfRecord = stoi(currentBlock.substr(0, 2));
-    // int recordLength = stoi(currentBlock.substr(startOfRecord, 2)) + 3; // plus three since `LI,` does not include itself
-    
-    // cout << "Substring at start: " << currentBlock.substr(startOfRecord, 15) << "\n";
-    // cout << "First record length: " << recordLength << "\n";
-    // int endOfBlock = currentBlock.find('~');
-    // cout << "Whole first record: " << currentBlock.substr(startOfRecord, recordLength) << "\n";
-    // cout << "End of block: " << endOfBlock<< "\n";
-    // cout << 
-
-    // actual program
     while (getline(readFile, currentBlock)) {
         // for each line, we have to skip the metadata!  
         int start = stoi(currentBlock.substr(0, 2));
@@ -74,16 +59,13 @@ int main() {
         while (start < endOfBlock) {
             size_t recordLength;
             try {
-                // cout << "recordLength: " << currentBlock.substr(start, 2) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
                 recordLength = stoi(currentBlock.substr(start, 2)) + 3; // plus three since `LI,` does not include itself
             } catch (const std::invalid_argument& ia) {
                 cerr << "Invalid argument: " << ia.what() << " for record length at block " << blockNumber << "\n";
                 return 1;
             }
-            // cout << "Whole current record: " << currentBlock.substr(start, recordLength) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
 
             string currentRecord = currentBlock.substr(start, recordLength);
-            // cout << "currentRecord: " << currentRecord << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
             int currentZipcode = findZipcode(currentRecord);
 
             if (currentZipcode > maxZipcode) {
