c0dc1ae1d3208a0079705b0174d22a6d03b78f83
Andrew Clayton <126101983+andrew-clayton@users.noreply.github.com>
2023-11-14 08:48:28 -0600

    Working index generation
    The index file generation program now works. I am committing this version so I have all of my troubleshooting measures present if I need to fix anything later on.

--- block_idx_gen.cpp
+++ block_idx_gen.cpp
@@ -27,12 +27,13 @@ int findZipcode(const string& record) {
     size_t firstComma = record.find(',');
     size_t secondComma = record.find(',', firstComma + 1);
     if (firstComma == string::npos || secondComma == string::npos) {
-        cerr << "Error parsing record for zipcode: " << record << endl;
+        cerr << "Error parsing record for zipcode: " << record << "\n";
         return -1;
     }
-    cout << "Zipcode: " << record.substr(firstComma + 1, secondComma - firstComma - 1) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
+    // cout << "Zipcode: " << record.substr(firstComma + 1, secondComma - firstComma - 1) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
     return stoi(record.substr(firstComma + 1, secondComma - firstComma - 1));
 }
+
 
 int main() {
     ofstream writeFile("blockedIndex.txt");
@@ -51,24 +52,38 @@ int main() {
     int maxZipcode = 0;
     string currentBlock;
 
+    // testing...
+    // getline(readFile, currentBlock);
+    // cout << "\n" << currentBlock << "\n\n";
+    // int startOfRecord = stoi(currentBlock.substr(0, 2));
+    // int recordLength = stoi(currentBlock.substr(startOfRecord, 2)) + 3; // plus three since `LI,` does not include itself
+    
+    // cout << "Substring at start: " << currentBlock.substr(startOfRecord, 15) << "\n";
+    // cout << "First record length: " << recordLength << "\n";
+    // int endOfBlock = currentBlock.find('~');
+    // cout << "Whole first record: " << currentBlock.substr(startOfRecord, recordLength) << "\n";
+    // cout << "End of block: " << endOfBlock<< "\n";
+    // cout << 
+
+    // actual program
     while (getline(readFile, currentBlock)) {
         // for each line, we have to skip the metadata!  
-        int start = stoi(currentBlock.substr(0, 2)) + 1;
-        cout << "Start: " << start << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf 
-        size_t end = currentBlock.find('~'); // Assuming '~' is the padding character
+        int start = stoi(currentBlock.substr(0, 2));
+        size_t endOfBlock = currentBlock.find('~'); // Assuming '~' is the padding character
 
-        while (start < end) {
+        while (start < endOfBlock) {
             size_t recordLength;
             try {
-                cout << "recordLength: " << currentBlock.substr(start, 2) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
-                recordLength = stoi(currentBlock.substr(start, 2));
+                // cout << "recordLength: " << currentBlock.substr(start, 2) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
+                recordLength = stoi(currentBlock.substr(start, 2)) + 3; // plus three since `LI,` does not include itself
             } catch (const std::invalid_argument& ia) {
-                cerr << "Invalid argument: " << ia.what() << " for record length at block " << blockNumber << endl;
+                cerr << "Invalid argument: " << ia.what() << " for record length at block " << blockNumber << "\n";
                 return 1;
             }
+            // cout << "Whole current record: " << currentBlock.substr(start, recordLength) << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
 
             string currentRecord = currentBlock.substr(start, recordLength);
-            cout << "currentRecord: " << currentRecord << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
+            // cout << "currentRecord: " << currentRecord << "\n"; ///////////////////////////////////////////////troubleohsinsiosd jjfsodij oasidjf
             int currentZipcode = findZipcode(currentRecord);
 
             if (currentZipcode > maxZipcode) {
@@ -77,7 +92,7 @@ int main() {
             start += recordLength;
         }
 
-        writeFile << blockNumber << "," << maxZipcode << endl;
+        writeFile << blockNumber << "," << maxZipcode << "\n";
 
         blockNumber++;
         maxZipcode = 0;
@@ -87,6 +102,3 @@ int main() {
     writeFile.close();
     return 0;
 }
-
-
-///abcdefghijklmnopqrstuvwxyz
